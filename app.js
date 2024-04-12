074ca2f4c3de4686aa6a9e755ce5a885383dc172
Cyclmobile <contact.cycl@gmail.com>
2024-04-12 21:35:50 +0200

    feat:check every 1 sec

--- app.js
+++ app.js
@@ -43,24 +43,24 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 
   function calculateDistance(coords1, coords2) {
-    const toRad = (x) => (x * Math.PI) / 180;
     const R = 6371e3; // Earth's radius in meters
-    const dLat = toRad(coords2.lat - coords1.lat);
-    const dLon = toRad(coords2.lng - coords1.lng);
+    const φ1 = (coords1.lat * Math.PI) / 180;
+    const φ2 = (coords2.lat * Math.PI) / 180;
+    const Δφ = ((coords2.lat - coords1.lat) * Math.PI) / 180;
+    const Δλ = ((coords2.lng - coords1.lng) * Math.PI) / 180;
+
     const a =
-      Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-      Math.cos(toRad(coords1.lat)) *
-        Math.cos(toRad(coords2.lat)) *
-        Math.sin(dLon / 2) *
-        Math.sin(dLon / 2);
+      Math.sin(Δφ / 2) * Math.sin(Δφ / 2) +
+      Math.cos(φ1) * Math.cos(φ2) * Math.sin(Δλ / 2) * Math.sin(Δλ / 2);
     const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    return Math.round(R * c);
+
+    return R * c; // Distance in meters
   }
 
   function locateUser() {
     const options = {
       enableHighAccuracy: true,
-      timeout: 5000, // 5 seconds
+      timeout: 1000, // 5 seconds
       maximumAge: 0,
     };
 
